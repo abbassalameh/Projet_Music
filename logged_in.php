f342390d07ec41d2ca4e4389d414d1cf2b636eb5
yuri-gh <yurigh00@gmail.com>
2013-09-15 13:55:02 +0300

    Coding Sunday

--- logged_in.php
+++ logged_in.php
@@ -32,6 +32,7 @@ if (empty ( $_SESSION ['username'] ) && empty ( $_SESSION ['password'] )) {
 <script type="text/javascript"
 	src="./plugin/jquery-jplayer/jquery.jplayer.js"></script>
 <script type="text/javascript" src="./plugin/ttw-music-player-min.js"></script>
+<script type="text/javascript" src="js/master_input.js"></script>
 <script type="text/javascript">
 	var myPlaylist = [
 		<?php
@@ -97,22 +98,46 @@ if (empty ( $_SESSION ['username'] ) && empty ( $_SESSION ['password'] )) {
 			<div class="right">
 				<section id="right">
 					<div class="content_right">
-						<div class="playlists">
-							<div class="title_container">
-								<div class="playlists_title">Playlists</div>
-							</div>
-						<div class="playlist_list">	
+						<form action="logged_in.php" method=GET>
+							<div class="playlists">
+								<div class="title_container">
+									<div class="playlists_title">Playlists</div>
+								</div>
+								<div class="playlist_list">
+									<div class="new_playlist">
+										<input type='button' name="add_playlist" class='add_playlist'
+											value='Add playlist'></input>
+									</div>
 					<?php
 					$playlist_query = "SELECT * FROM playlist WHERE username='" . $_SESSION ['username'] . "'";
 					$result_playlist = mysql_query ( $playlist_query );
-					while ( $row_playlists = mysql_fetch_assoc ( $result_playlist ) ) {
-						echo "<div class=\"playlist_name\"><a href=\"playlist.php\">" . $row_playlists ['playlist_name'] . "</a></div>";
-						$_SESSION['playlist_name']=$row_playlists['playlist_name'];
+					if (isset ( $_GET ['submit_playlist_name'] )) {
+						if (! empty ( $_GET ['playlist_name_input'] )) {
+							$playlist_entry = $_GET ['playlist_name_input'];
+							$playlist_insert_query = "INSERT INTO playlist(id,username,playlist_name) VALUES ('' ,'" . $_SESSION ['username'] . "','" . $playlist_entry . "')";
+							$check_playlist = "SELECT * FROM playlist WHERE playlist_name = '$playlist_entry'";
+							$playlist_checking = mysql_query ( $check_playlist );
+							$nb_results = mysql_num_rows ( $playlist_checking );
+							if ($nb_results > 0)
+								"<div class=playlist_exists>You alreay a playlist named " . $playlist_entry . "</div>";
+							
+							else
+								mysql_query ( $playlist_insert_query );
+						}
 					}
-						
+					$result_playlist = mysql_query ( $playlist_query );
+					if (mysql_num_rows ( $result_playlist ) > 0) {
+						while ( $row_playlists = mysql_fetch_assoc ( $result_playlist ) ) {
+							echo "<div class=\"playlist_name\"><a href=\"playlist.php\">" . $row_playlists ['playlist_name'] . "</a></div>";
+							$_SESSION ['playlist_name'] = $row_playlists ['playlist_name'];
+						}
+					} else
+						"<div class='playlist_emtpy'>Your playlist is empty</div>";
+					
 					?>
 					</div>
-					</div>
+							</div>
+						</form>
 					</div>
 				</section>
 			</div>
